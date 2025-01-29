00ba41035516860b8eafe51e04ccf8d7d0bab05e
LyddaG <pl241701@ahschool.com>
2025-01-29 08:10:44 -0500

    yeahah

--- window.py
+++ window.py
@@ -1,5 +1,10 @@
 import tkinter as tk
+from tkinter import filedialog as fd
 # tk._test()
+
+
+def openFiles():
+    BWimage = fd.askopenfilename(title= "Provide a black and white PNG (TRANSPARENT IS NOT WHITE!)", filetypes= "*.png")
 
 source = tk.Tk()
 
@@ -8,5 +13,7 @@ source.minsize(340, 200)
 source.geometry("300x300+100+100")
 
 tk.Label(source, text="Experimentation").pack()
+
+tk.Button(source, text="Enter photo", command= openFiles).pack()
 
 source.mainloop()
