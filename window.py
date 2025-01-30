e5ef5c53fd32369773c7c6af7e138eeae12bdbf5
LyddaG <pl241701@ahschool.com>
2025-01-30 08:45:18 -0500

    trying opening photo

--- window.py
+++ window.py
@@ -1,10 +1,18 @@
 import tkinter as tk
 from tkinter import filedialog as fd
+from tkinter import PhotoImage as PI
+from PIL import Image, ImageTK
 # tk._test()
 
 
+
 def openFiles():
-    BWimage = fd.askopenfilename(title= "Provide a black and white PNG (TRANSPARENT IS NOT WHITE!)", filetypes= "*.png")
+    BWimage = fd.askopenfilename(title= "Provide a black and white PNG (TRANSPARENT IS NOT WHITE!)", filetypes= [("PNG files", "*.png")])
+    if BWimage:
+        with open(BWimage, "rb") as file:
+            content = Image.open(file)
+            tk.Label(source, content).pack()
+
 
 source = tk.Tk()
 
@@ -15,5 +23,7 @@ source.geometry("300x300+100+100")
 tk.Label(source, text="Experimentation").pack()
 
 tk.Button(source, text="Enter photo", command= openFiles).pack()
+
+
 
 source.mainloop()
