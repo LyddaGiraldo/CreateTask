1007a30167459eb2dfaa20d9ba8e6f0d1b534354
LyddaG <pl241701@ahschool.com>
2025-02-13 08:43:36 -0500

    made photo work yippie

--- window.py
+++ window.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import filedialog as fd
+from tkinter import PhotoImage as PI
 from PIL import Image, ImageTk
 
 # tk._test()
@@ -10,21 +11,33 @@ class MainWindow():
         self.source = source
         self.source.title("Testing")
         self.source.minsize(340, 200)
-        self.source.geometry("300x300+100+100")
+        self.source.geometry("500x300+100+100")
         
-        self.BWImage = Image.new("1",[100,100])
+        defaultImage = Image.open("defaultgrey.png")
+        defaultImage = defaultImage.resize((250,250))
+        defaultImage = ImageTk.PhotoImage(defaultImage)
 
-        self.imageToTextLabel = tk.Label(self.source, text="Experimentation").pack()
-        self.importImage = tk.Button(self.source, text="Enter photo", command= self.openFiles).pack()
-        self.BWImageDisplay = tk.Label(source, text="test", image= ImageTk.PhotoImage(self.BWImage)).pack()
+
+        self.imageToTextLabel = tk.Label(self.source, text="Experimentation")
+        self.imageToTextLabel.pack()
+        
+        self.BWImageDisplay = tk.Label(source, text="test", image= defaultImage)
+        self.BWImageDisplay.defaultImage=defaultImage
+        self.BWImageDisplay.pack(side= "left")
+
+        self.importImage = tk.Button(self.source, text="Enter photo", command= self.openFiles)
+        self.importImage.pack(side= "left")
         
     def openFiles(self):
     
         image = fd.askopenfilename(title= "Provide a black and white PNG (TRANSPARENT IS NOT WHITE!)", filetypes= [("PNG files", "*.png")])
-        
+
         image = Image.open(image)
+    
+
+        image = image.resize((250,250))
         self.BWimage = ImageTk.PhotoImage(image)
-        self.BWImageDisplay.config(image=self.BWimage)
+        self.BWImageDisplay.configure(image=self.BWimage)
         print("imagefound")
         
         
